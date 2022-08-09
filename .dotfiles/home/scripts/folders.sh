05e86178237b30a01391c681ae574829dca5e418
liveofit <ibarretorey@gmail.com>
2022-08-08 21:26:18 -0300

    fix add neovim ppa repository

--- .dotfiles/home/scripts/folders.sh
+++ .dotfiles/home/scripts/folders.sh
@@ -4,7 +4,7 @@
 # them through `fzf`. The returning value can be used as input for other
 # scripts. For example, to generate new tmux windows or sessions.
 {
-  find ~ -mindepth 1 -maxdepth 1 -type d & \
-  find ~/Documents -mindepth 1 -maxdepth 1 -type d & \
-  find ~/Documents/projects -mindepth 1 -maxdepth 4 -type d ! -path '*/.git' ! -path '*/.git/*' ! -path '*/.node_modules' ! -path '*/.node_modules/*' | grep -E ".*/projects/*" & \
+    find ~ -mindepth 1 -maxdepth 1 -type d &
+    find ~/Documents -mindepth 1 -maxdepth 1 -type d &
+    find ~/Documents/projects -mindepth 1 -maxdepth 4 -type d ! -path '*/.git' ! -path '*/.git/*' ! -path '*/.node_modules' ! -path '*/.node_modules/*' | grep -E ".*/projects/*" &
 } | sort -u | fzf-tmux -p 90%,90%
