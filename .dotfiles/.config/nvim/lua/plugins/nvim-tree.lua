9084c66faa617f9890bd1b35e2c9172345d5802a
liveonit <ibarretorey@gmail.com>
2023-11-02 15:34:00 -0300

    add custom mapping to nvim-tree

--- .dotfiles/.config/nvim/lua/plugins/nvim-tree.lua
+++ .dotfiles/.config/nvim/lua/plugins/nvim-tree.lua
@@ -1,9 +1,21 @@
--- empty setup using defaults
-require("nvim-tree").setup()
+local function my_on_attach(bufnr)
+  local api = require "nvim-tree.api"
 
--- OR setup with some options
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- custom mappings
+  -- vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent)
+end
+
+
 require("nvim-tree").setup({
 	sort_by = "case_sensitive",
+  on_attach = my_on_attach,
 	view = {
 		width = 30,
 	},
@@ -14,3 +26,5 @@ require("nvim-tree").setup({
 		dotfiles = false,
 	},
 })
+
+vim.keymap.set('n', '<leader>tf', '<cmd>NvimTreeFocus<CR>')
