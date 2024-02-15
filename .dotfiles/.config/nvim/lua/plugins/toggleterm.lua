c152da9de4146655b892b84a5c0b49eec5476170
liveonit <ibarretorey@gmail.com>
2024-02-15 20:08:36 -0300

    fixed issues with toogleterm next dirs

--- .dotfiles/.config/nvim/lua/plugins/toggleterm.lua
+++ .dotfiles/.config/nvim/lua/plugins/toggleterm.lua
@@ -12,76 +12,57 @@ end
 
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
-local lazygit = Terminal:new({
-  cmd = "lazygit",
-  dir = "git_dir",
-  direction = "float",
-  float_opts = {
-    border = "double",
-  },
-  -- function to run on opening the terminal
-  on_open = function(term)
-    vim.cmd("startinsert!")
-    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
-  end,
-  -- function to run on closing the terminal
-  on_close = function(term)
-    vim.cmd("startinsert!")
-  end,
-})
+
 
 function _lazygit_toggle()
+  print(vim.fn.getcwd())
+  local lazygit = Terminal:new({
+    cmd = "lazygit",
+    dir = vim.fn.getcwd(),
+    direction = "float",
+    float_opts = {
+      border = "double",
+    },
+    -- function to run on opening the terminal
+    on_open = function(term)
+      vim.cmd("startinsert!")
+      vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+    end,
+    -- function to run on closing the terminal
+    on_close = function(term)
+      vim.cmd("startinsert!")
+    end,
+  })
   lazygit:toggle()
 end
 
 vim.api.nvim_set_keymap("n", "<leader>tg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
 
 
-local htop = Terminal:new({
-  cmd = "htop",
-  dir = "git_dir",
-  direction = "float",
-  float_opts = {
-    border = "double",
-  },
-  -- function to run on opening the terminal
-  on_open = function(term)
-    vim.cmd("startinsert!")
-    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
-  end,
-  -- function to run on closing the terminal
-  on_close = function(term)
-    vim.cmd("startinsert!")
-  end,
-})
+
 
 function _htop_toggle()
+  local htop = Terminal:new({
+    cmd = "htop",
+    dir = vim.fn.getcwd(),
+    direction = "float",
+    float_opts = {
+      border = "double",
+    },
+    -- function to run on opening the terminal
+    on_open = function(term)
+      vim.cmd("startinsert!")
+      vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+    end,
+    -- function to run on closing the terminal
+    on_close = function(term)
+      vim.cmd("startinsert!")
+    end,
+  })
   htop:toggle()
 end
 
 vim.api.nvim_set_keymap("n", "<leader>tt", "<cmd>lua _htop_toggle()<CR>", { noremap = true, silent = true })
-
-
-
-local function dump(o)
-  if type(o) == 'table' then
-    local s = '{ '
-    for k, v in pairs(o) do
-      if type(k) ~= 'number' then k = '"' .. k .. '"' end
-      s = s .. '[' .. k .. '] = ' .. dump(v) .. ','
-    end
-    return s .. '} '
-  else
-    return tostring(o)
-  end
-end
-
-local getPath = function(str, sep)
-  sep = sep or '/'
-  return str:match("(.*" .. sep .. ")")
-end
-
-
 
 
 function _pnpm_eslint_toggle()
@@ -109,7 +90,6 @@ function _pnpm_eslint_toggle()
       end
     end,
   })
-
   pnpm_eslint:toggle()
 end
 
