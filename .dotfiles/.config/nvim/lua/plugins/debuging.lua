7b1d23e9f102348c23fc1ec7ae9f2710cb5808b1
liveonit <ibarretorey@gmail.com>
2024-07-26 16:19:02 -0300

    added script to allow dap to auto load configs from launch.json when the file is ts or js

--- .dotfiles/.config/nvim/lua/plugins/debuging.lua
+++ .dotfiles/.config/nvim/lua/plugins/debuging.lua
@@ -1,71 +1,86 @@
 local dap = require("dap")
+local utils = require("utils")
 
 -- Set keymaps to control the debugger
-vim.keymap.set('n', 'dc', dap.continue)
-vim.keymap.set('n', 'dj', dap.step_over)
-vim.keymap.set('n', 'dl', dap.step_into)
-vim.keymap.set('n', 'dh', dap.step_out)
+vim.keymap.set('n', '<leader>dc', function()
+  local launch_config_dir = utils.find_ancestor_with_file("launch.json")
+  if launch_config_dir and utils.get_file_extension(vim.fn.expand("%:p")) == "ts" then
+    local launch_config = utils.read_and_parse_json_file(launch_config_dir .. "/launch.json")
+    if not (utils.table_contains_by_attribute(dap.configurations.typescript, "name", launch_config.name)) then
+      dap.configurations.typescript[#dap.configurations.typescript + 1] = launch_config
+    end
+  end
+
+  if launch_config_dir and utils.get_file_extension(vim.fn.expand("%:p")) == "js" then
+    dap.configurations.javascript[#dap.configurations.javascript + 1] = utils.read_and_parse_json_file(launch_config_dir ..
+      "/launch.json")
+  end
+
+  dap.continue()
+end)
+vim.keymap.set('n', '<leader>dj', dap.step_over)
+vim.keymap.set('n', '<leader>dl', dap.step_into)
+vim.keymap.set('n', '<leader>dh', dap.step_out)
 vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint)
 vim.keymap.set('n', '<leader>B', function()
   require 'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))
 end)
-vim.fn.sign_define('DapBreakpoint',{ text ='📌', texthl ='', linehl ='', numhl =''})
-vim.fn.sign_define('DapStopped',{ text ='👉', texthl ='', linehl ='', numhl =''})
+vim.fn.sign_define('DapBreakpoint', { text = '📌', texthl = '', linehl = '', numhl = '' })
+vim.fn.sign_define('DapStopped', { text = '👉', texthl = '', linehl = '', numhl = '' })
 
 
 require("mason-nvim-dap").setup({
-    ensure_installed = { "python", "cppdbg", "codelldb", "node2", "js", "chrome", "firefox", "bash" },
-    automatic_installation = true,
+  ensure_installed = { "python", "cppdbg", "codelldb", "node2", "js", "chrome", "firefox", "bash" },
+  automatic_installation = true,
 })
 
 -- Setup node-debug2-adapter
-require('dap').adapters.node2 = {
- type = "executable",
- command = "node",
- args = {
-  require("mason-registry").get_package("node-debug2-adapter"):get_install_path() .. "/out/src/nodeDebug.js",
- },
+dap.adapters.node2 = {
+  type = "executable",
+  command = "node",
+  args = {
+    require("mason-registry").get_package("node-debug2-adapter"):get_install_path() .. "/out/src/nodeDebug.js",
+  },
 }
 
 -- Setup firefox-debug-adapter
-require('dap').adapters.firefox = {
-    type = "executable",
-    command = "node",
-    args = {
-        require("mason-registry").get_package("firefox-debug-adapter"):get_install_path() .. "/dist/adapter.bundle.js",
-    },
+dap.adapters.firefox = {
+  type = "executable",
+  command = "node",
+  args = {
+    require("mason-registry").get_package("firefox-debug-adapter"):get_install_path() .. "/dist/adapter.bundle.js",
+  },
 }
 
 -- Configuration of the adapters for the respective language
-
- require('dap').configurations.javascript = {
+dap.configurations.javascript = {
   {
-   name = "Launch Node against current file",
-   type = "node2",
-   request = "launch",
-   program = "${file}",
-   cwd = "${workspaceFolder}",
-   sourceMaps = true,
-   protocol = "inspector",
-   console = "integratedTerminal",
+    name = "Launch Node against current file",
+    type = "node2",
+    request = "launch",
+    program = "${file}",
+    cwd = "${workspaceFolder}",
+    sourceMaps = true,
+    protocol = "inspector",
+    console = "integratedTerminal",
   },
   {
-   name = "Launch Node against pick process",
-   type = "node2",
-   request = "attach",
-   processId = require("dap.utils").pick_process,
-   console = "integratedTerminal",
+    name = "Launch Node against pick process",
+    type = "node2",
+    request = "attach",
+    processId = require("dap.utils").pick_process,
+    console = "integratedTerminal",
   },
   {
-   name = "Launch Firefox against localhost",
-   request = "launch",
-   type = "firefox",
-   reAttach = true,
-   url = "http://localhost:3000",
-   webRoot = "${workspaceFolder}",
-   console = "integratedTerminal",
+    name = "Launch Firefox against localhost",
+    request = "launch",
+    type = "firefox",
+    reAttach = true,
+    url = "http://localhost:3000",
+    webRoot = "${workspaceFolder}",
+    console = "integratedTerminal",
   },
- }
+}
 
 dap.configurations.typescript = {
   {
@@ -77,22 +92,24 @@ dap.configurations.typescript = {
     args = { "--inspect", "${file}" },
     skipFiles = { "node_modules/**" },
     console = "integratedTerminal",
-  },
+  }
 }
 
 for _, language in pairs({ "javascriptreact", "typescriptreact" }) do
- require('dap').configurations[language] = {
-  {
-   name = "Launch Firefox against localhost",
-   request = "launch",
-   type = "firefox",
-   reAttach = true,
-   url = "http://localhost:3000",
-   webRoot = "${workspaceFolder}",
-   console = "integratedTerminal",
-  },
- }
+  require('dap').configurations[language] = {
+    {
+      name = "Launch Firefox against localhost",
+      request = "launch",
+      type = "firefox",
+      reAttach = true,
+      url = "http://localhost:3000",
+      webRoot = "${workspaceFolder}",
+      console = "integratedTerminal",
+    },
+  }
 end
+
+
 
 local dapui = require("dapui")
 
@@ -110,4 +127,3 @@ end
 dap.listeners.before.event_exited.dapui_config = function()
   dapui.close()
 end
-
--- .dotfiles/.config/nvim/lua/plugins/lsp-cmp.lua
+++ .dotfiles/.config/nvim/lua/plugins/lsp-cmp.lua
@@ -67,9 +67,6 @@ local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
 cmp.setup({
   sources = {
-    -- Copilot Source
-    -- { name = "copilot",                 group_index = 2 },
-    -- Other Sources
     { name = "nvim_lsp",                group_index = 2 },
     { name = "path",                    group_index = 2 },
     { name = "nvim_lua",                group_index = 2 },
